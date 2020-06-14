c6338c8a8369edf117d915aabdca58c212df447a
Marius J. H. Johansen <twiller3@hotmail.com>
2020-06-14 18:58:20 +0200

    portal page, signin component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from './pages/homepage/homepage'
 import { Route, Switch } from 'react-router-dom'
 import ShopPage from './pages/shop/shop' 
 import Header from './components/Header/header'
+import Portal from './pages/portal/portal'
 
 
 const HatsPage = () => (
@@ -23,6 +24,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path ='/portal' component={Portal} />
       </Switch>
     </div>
   );
---
+++ src/pages/portal/portal.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './portal.style.scss'
+
+import SignIn from '../../components/signIn/signIn'
+
+const Portal = () => (
+    <div className='portal-container'>
+        <SignIn />
+    </div>
+)
+
+export default Portal;
